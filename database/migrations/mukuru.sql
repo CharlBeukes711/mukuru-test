e17af9334eee38bbf9c08f33e5cc96e8574e534d
CharlBeukes711 <charlbeukes@gmail.com>
2016-03-18 06:18:49 +0200

    removed old migration files

--- database/migrations/mukuru.sql
+++ database/migrations/mukuru.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 14, 2016 at 04:35 PM
+-- Generation Time: Mar 18, 2016 at 05:09 AM
 -- Server version: 5.7.11-log
 -- PHP Version: 5.5.12
 
@@ -33,44 +33,58 @@ CREATE TABLE IF NOT EXISTS `currencies` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `code` varchar(5) COLLATE utf8_unicode_ci NOT NULL,
   `description` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `exchange_rate` double NOT NULL,
-  `surcharge` double NOT NULL,
+  `exchange_rate` float NOT NULL,
+  `surcharge` float NOT NULL,
+  `discount_percentage` float NOT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=5 ;
 
 --
+-- Truncate table before insert `currencies`
+--
+
+TRUNCATE TABLE `currencies`;
+--
 -- Dumping data for table `currencies`
 --
 
-INSERT INTO `currencies` (`id`, `code`, `description`, `exchange_rate`, `surcharge`, `created_at`, `updated_at`) VALUES
-(1, 'USD', 'US Dollars', 0.0808279, 7.5, '2016-03-14 00:00:00', NULL),
-(2, 'GBP', 'British Pound', 0.0527032, 5, '2016-03-14 00:00:00', NULL),
-(3, 'EUR', 'Euro', 0.071871, 5, '2016-03-14 00:00:00', NULL),
-(4, 'KES', 'Kenyan Shilling ', 7.81498, 2.5, '2016-03-14 00:00:00', NULL);
+INSERT INTO `currencies` (`id`, `code`, `description`, `exchange_rate`, `surcharge`, `discount_percentage`, `created_at`, `updated_at`) VALUES
+(1, 'USD', 'US Dollars', 0.0808279, 7.5, 0, '2016-03-14 00:00:00', '2016-03-18 03:44:10'),
+(2, 'GBP', 'British Pound', 0.0527032, 5, 0, '2016-03-14 00:00:00', '2016-03-18 03:44:10'),
+(3, 'EUR', 'Euro', 0.071871, 5, 2, '2016-03-14 00:00:00', '2016-03-18 03:44:10'),
+(4, 'KES', 'Kenyan Shilling ', 7.81498, 2.5, 0, '2016-03-14 00:00:00', '2016-03-18 03:44:10');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `migrations`
+-- Table structure for table `orders`
 --
 
-DROP TABLE IF EXISTS `migrations`;
-CREATE TABLE IF NOT EXISTS `migrations` (
-  `migration` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `batch` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE IF NOT EXISTS `orders` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned DEFAULT NULL,
+  `currency_id` int(10) unsigned NOT NULL,
+  `exchange_rate` float NOT NULL,
+  `surcharge_percentage` float NOT NULL,
+  `surcharge_amount` float NOT NULL,
+  `foreign_currency_amount` float NOT NULL,
+  `zar_amount` float NOT NULL,
+  `discount_percentage` float NOT NULL,
+  `discount_amount` float NOT NULL,
+  `total_amount` float NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 --
--- Dumping data for table `migrations`
+-- Truncate table before insert `orders`
 --
 
-INSERT INTO `migrations` (`migration`, `batch`) VALUES
-('2014_10_12_000000_create_users_table', 1),
-('2014_10_12_100000_create_password_resets_table', 1),
-('2016_03_14_093612_create_currencies_table', 2);
-
+TRUNCATE TABLE `orders`;
 -- --------------------------------------------------------
 
 --
@@ -86,6 +100,11 @@ CREATE TABLE IF NOT EXISTS `password_resets` (
   KEY `password_resets_token_index` (`token`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Truncate table before insert `password_resets`
+--
+
+TRUNCATE TABLE `password_resets`;
 -- --------------------------------------------------------
 
 --
@@ -99,12 +118,17 @@ CREATE TABLE IF NOT EXISTS `users` (
   `email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `password` varchar(60) COLLATE utf8_unicode_ci DEFAULT NULL,
   `remember_token` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `users_email_unique` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
+--
+-- Truncate table before insert `users`
+--
+
+TRUNCATE TABLE `users`;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
